31755d1adc6726b1cc58d6221d6837d57881affa
earthbot <earthbot@localhost>
2017-02-02 13:29:40 -0500

    Have very basic movement working

--- starry/starry.js
+++ starry/starry.js
@@ -1,3 +1,5 @@
+var player;
+
 var
   WIDTH = window.innerWidth,
   HEIGHT = window.innerHeight,
@@ -9,21 +11,37 @@ function preload() {
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  
+  player = new Player();
 }
 
 function draw() {
   background(0, 0, 0);
+  
+  player.display();
 }
 
 function keyPressed() {
   if(keyCode === ENTER) {
     console.log("Hey");
   }
+  if(keyCode == 37) {
+    console.log("Left");
+    player.x--;
+  }
 }
 
 function windowResized() {
-WIDTH = window.innerWidth,
-HEIGHT = window.innerHeight,
-resizeCanvas(WIDTH, HEIGHT);
+  WIDTH = window.innerWidth,
+  HEIGHT = window.innerHeight,
+  resizeCanvas(WIDTH, HEIGHT);
+}
+
+function Player() {
+  this.x = 200;
+  this.y = 200;
+  
+  this.display = function() {
+    //color(255, 255, 255);
+    ellipse(this.x, this.y, 20, 20);
+  }
 }
