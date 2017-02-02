3d611d5fa06f0af6436d818decf14d3a51056317
earthbot <earthbot@localhost>
2017-02-02 12:40:57 -0500

    basically have skeleton done

--- starry/starry.js
+++ starry/starry.js
@@ -1,5 +1,15 @@
-function setup() {
+var
+  WIDTH = window.innerWidth,
+  HEIGHT = window.innerHeight,
+  FPS = 60; // Frames per second
 
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT);
+  
 }
 
 function draw() {
