2ec1688f9970b28f1edbcd7dd8c6f8f9f4d62c68
earthbot <earthbot@localhost>
2017-02-07 09:42:25 -0500

    Got basic movement working, am going to switch to using vecotrs

--- starry/starry.js
+++ starry/starry.js
@@ -1,3 +1,10 @@
+/*
+
+TODO:
+Stop using an angle, and just use a vector for both position and rotation
+
+*/
+
 var player;
 
 var
@@ -18,21 +25,49 @@ function setup() {
 function draw() {
   background(0, 0, 0);
   
+  handleInput();
+
   player.display();
 }
 
-function keyPressed() {
-  if(keyCode === ENTER) {
-    console.log("Hey");
+function handleInput() {
+  if(keyIsDown(LEFT_ARROW)) {
+    player.angle -= 10;
   }
-  if(keyCode == 37) {
-    console.log("Left");
-    player.x--;
+  if(keyIsDown(RIGHT_ARROW)) {
+    player.angle += 10;
+  }
+  if(keyIsDown(UP_ARROW)) {
+    console.log("Thrust");
   }
 }
 
+
+// Going to handle this with keyIsDown instead, but don't like it being
+// in the draw() function, as I can't rely on draw() to be called consistently.
+/*
+function keyPressed() {
+  if(keyCode === ENTER) {
+    //console.log("Hey");
+    player.angle = PI/3;
+  }
+  if(keyCode == 37) {
+    console.log("Left");
+    //player.x--;
+    player.angle -= 20;
+    
+  }
+}
+*/
+
 function mouseMoved() {
-    console.log(mouseX);
+    console.log(mouseX, lastMouseX);
+    if(lastMouseX < mouseX)
+      player.angle += 20;
+    else if(lastMouseX > mouseX)
+      player.angle -= 20;
+      
+    lastMouseX = mouseX;
 }
 
 function windowResized() {
@@ -42,14 +77,28 @@ function windowResized() {
 }
 
 function Player() {
-  this.x = 20;
-  this.y = 20;
+  this.x = 0;
+  this.y = 0;
   this.angle = 0;
+  //this.velocity = createVector(0, 0).limit(5);
+  this.velocity = 0;
+  
+  this.update = function() {
+    
+  }
   
   this.display = function() {
     push();
     //ellipse(this.x, this.y, 20, 20);
-    triangle(this.x, this.y - 20, this.x - 10, this.y, this.x + 10, this.y);
+    //rotate(this.angle);
+    translate(30, 30);
+    rotate(radians(this.angle));
+    //translate(30, 30);
+    //translate(width/2, height/2);
+    triangle(this.x, this.y - 15, this.x - 10, this.y + 10, this.x + 10, this.y + 10);
+    //fill(255, 0, 0);
+    //rect(this.x, this.y, 10, 10);
+    //ellipse(this.x, this.y, 10, 10);
     pop();
   }
 }
